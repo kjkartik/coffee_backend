fdda1d1e07978ac8f9f76545ef3a404c42d2a07f
kartik Sharma <kartiksharma@kartiks-MacBook-Air.local>
2025-01-13 01:01:46 +0530

    deploy to server_1

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const { sendFail, sendError } = require("./failureHandling");
-const { sendSuccess } = require("./sucessHandling");
+const { sendFail, sendError }  = require("./failureHandling");
+const { sendSuccess }  = require("./sucessHandling");
 
 module.exports = { sendFail, sendError, sendSuccess };
